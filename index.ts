ed3d216fad972b62e33b87fe87b8625d57a97e1b
Hikomi-wg <salikhova.lena.m@gmail.com>
2026-09-21 11:24:18 +0300

    Базовая структура меню (скелетик)

---
+++ index.ts
@@ -0,0 +1,14 @@
+function showMenu(): void {
+    console.log("=== МЕНЮ ===");
+    console.log("1. Ввести два числа");
+    console.log("2. Выполнить сложение");
+    console.log("3. Выполнить вычитание");
+    console.log("4. Выполнить деление");
+    console.log("5. Возвести число в степень");
+    console.log("0. Выход");
+}
+
+let num1: number = 0;
+let num2: number = 0;
+
+showMenu();
